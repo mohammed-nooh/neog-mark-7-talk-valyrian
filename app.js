d06830836658f7e89dcc19b84a6caff84a44cdeb
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-06-03 19:28:23 +0530

    updated JS

--- app.js
+++ app.js
@@ -1,27 +1,109 @@
 var btnTranslate=document.querySelector("#submit");
 
-var textInput=document.querySelector("text-area");
+var textInput=document.querySelector("#text-area");
 
+var outputDiv=document.querySelector("#output-box")
 
-textInput.addEventListener("change",testEvent)
+var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-function testEvent(){
-    
-var textValue=textInput.value;
-console.log(textValue);
-    
+function errorHandler(error){
+    alert("something is wrong please try again after some time or contact me ",error)
 }
 
+function constructUrl(text){
+    return url+"?text="+text;
+}
 
-
-
-
-
-
+function doFetch(text){
+    fetch(constructUrl(text))
+    .then(response => response.json())
+    .then(json =>{ outputDiv.innerText=json.contents.translated;
+        console.log(json)})
+    .catch(errorHandler)
+}
 
 
 btnTranslate.addEventListener("click",clickEventHandler)
 
 function clickEventHandler(){
-    console.log("clicked");
+    var input=textInput.value;
+    doFetch(input);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ===========================================================
+//  the function below is to console the input text whwnever we click the textarea 
+// ================================================
+
+
+// textInput.addEventListener("click",testEvent)
+
+// function testEvent(){
+    
+// var textValue=textInput.value;
+
+// if(textValue!=""){
+// console.log(textValue);
+// }
+    
+// }
+// =================
+//  function over 
+// =====================
+
+
+// ================================================
+// function to put the textInput to the outputDiv 
+// =====================================================
+
+// btnTranslate.addEventListener("click",output)
+
+// function output(){
+//     var textValue=textInput.value;
+//     outputDiv.innerText=textValue;
+//     console.log(textValue)
+// }
+
+// ======================
+// functon Over
+// =======================
+
+
+
+
+
+// btnTranslate.addEventListener("click",clickEventHandler)
+
+// function clickEventHandler(){
+//     console.log("clicked");
+// }
