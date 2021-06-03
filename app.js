9df964c91eb65a36bfeff99b5da848b409e84b28
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-06-03 15:07:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ var btnTranslate=document.querySelector("#submit");
 
 var textInput=document.querySelector("text-area");
 
-var textValue=textInput.value
-console.log(textValue)
+
+textInput.addEventListener("change",testEvent)
+
+function testEvent(){
+    
+var textValue=textInput.value;
+console.log(textValue);
+    
+}
+
+
+
+
+
+
+
 
 
 btnTranslate.addEventListener("click",clickEventHandler)
